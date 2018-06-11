11c1cfd36c1b9256b6014f3795edc5d22d5405f2
Joe Jobes <jmrjobes@gmail.com>
2018-06-11 10:20:10 -0600

    Add clock.rb for clockwork setup

---
+++ clock.rb
@@ -0,0 +1,14 @@
+require 'clockwork'
+require 'clockwork/database_events'
+require_relative './config/boot'
+require_relative './config/environment'
+
+module Clockwork
+
+  Clockwork.manager = DatabaseEvents::Manager.new
+
+  sync_database_events model: ::Event, every: 1.hour do |event|
+    event.job_name.constantize.perform_later()
+  end
+
+end
